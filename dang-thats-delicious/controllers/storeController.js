1425ed3a4314b3b09300985cf46a7a14dcd85e27
Henry Zarza <henryacz7@gmail.com>
2018-07-24 12:18:46 -0500

    Querying our database for Stores

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -13,6 +13,10 @@ exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
 
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-
     res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores });
 }
